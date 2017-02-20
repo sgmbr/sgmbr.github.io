ba399d769263f008209928e6eca9711573b0b3c4
sgmbr <satoshi.sgmr@gmail.com>
2017-02-20 23:00:56 +1300

    Add scroll feature

--- js/script.js
+++ js/script.js
@@ -3,4 +3,26 @@ $(document).ready(function() {
     $('.navbar-toggle').click(function() {
         $(this).next().slideToggle('fast');
     });
+
+    // scrolling
+    $('.navbar a').on('click', function(event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 600, function() {
+                window.location.hash = hash;
+            });
+        }
+    });
 })
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 1) {
+        $('header').addClass("sticky");
+    } else {
+        $('header').removeClass("sticky");
+    }
+});
